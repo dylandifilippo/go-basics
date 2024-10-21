7586743b39608b41d162f2bb99de462ad533d831
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-10-21 19:20:38 +0200

    Refactor project structure and add investment calculator functionality

--- interfaces/main.go
+++ interfaces/main.go
@@ -7,10 +7,39 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	saver
+	displayer
+}
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text:")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = outputData(todo)
+
+	if err != nil {
+		fmt.Println("Saving the todo failed.")
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +48,29 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(userNote)
 
 	if err != nil {
 		fmt.Println("Saving the note failed.")
 		return
 	}
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed.")
+		return err
+	}
 
 	fmt.Println("Saving the note succeeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
