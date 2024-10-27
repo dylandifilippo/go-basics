4457a5a7588a622954cadbb899e796eb60934f3b
Dylan Di Filippo <dylan.di.filippo@gmail.com>
2024-10-27 18:14:13 +0100

    Refactor project structure and update event creation and update functionality

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -80,3 +80,22 @@ func GetEventByID(id int64) (*Event, error) {
 
 	return &e, err
 }
+
+func (e Event) Update() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?, user_id = ?
+	WHERE id = ?
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID, e.ID)
+
+	return err
+}
